03eb75095e90f8d80abb1a7f5ba98634aa3625fc
“Nababa <nababa3@github.com>
2024-01-04 00:49:11 +0000

    ES6

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return [...reportWithIterator].join(' | ');
+  let employees = '';
+  for (const item of reportWithIterator) {
+    employees += `${item} | `;
+  }
+  return employees.slice(0, -3);
 }
