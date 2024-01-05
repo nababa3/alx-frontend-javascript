4d18ed6e2fbccb351e2d1ad9219fe2e64a4a7179
“Nababa <nababa3@github.com>
2024-01-05 01:05:39 +0000

    classes

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,19 +1,15 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw new TypeError('Brand must be a string');
-    } else if (typeof motor !== 'string') {
-      throw new TypeError('Motor must be a string');
-    } else if (typeof color !== 'string') {
-      throw new TypeError('Color must be a string');
-    }
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  cloneCar() {
+  static get [Symbol.species]() {
     return this;
+  }
+
+  cloneCar() {
+    return new this.constructor[Symbol.species]();
   }
 }
