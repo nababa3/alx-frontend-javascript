b7241c9e2074894a2ef5b9154ac76070df4c9eaa
“Nababa <nababa3@github.com>
2024-01-13 02:47:59 +0000

    ts

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,21 @@
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingJava?: number;
+    }
+
+    export class Java extends Subject {
+        getRequirements(): string {
+            return 'Here is the list of requirements for Java';
+        }
+
+        getAvailableTeacher(): string {
+            const teachingExpJava = this.teacher.experienceTeachingJava;
+            if (typeof teachingExpJava === 'number' && teachingExpJava > 0) {
+                return `Available Teacher: ${this.teacher.firstName}`;
+            } else {
+                return 'No available teacher';
+            }
+        }
+    }
+
+}
